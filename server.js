095765b3234186f34a86f5ca0dc6c46363cad4b5
Yiu Hei Chang <89966239+jackyyhc@users.noreply.github.com>
2023-10-10 23:44:08 +0800

    added student view and change infoformation part

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.post("/password_modification", (req, res) => {
   if (!req.session.authenticated) {
     res.redirect('/');
   } else if (req.session.role == 'teacher' || req.session.role == 'student') {
-    let sql = 'select password from 350_group_project_1.user where username = 12000000;';
-    connection.query(sql, (error, results, fields) => {
+    let sql = 'select password from 350_group_project_1.user where username = ?;';
+    connection.query(sql,[req.body.student_id], (error, results, fields) => {
       if (error) throw error;
       const orginal_pw = results[0].password;
       const username = req.body.student_id;
@@ -81,7 +81,42 @@ app.post("/password_modification", (req, res) => {
   }
 }
 );
+app.post("/submit-student-info", (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect('/');
+  } else if (req.session.role == 'teacher' || req.session.role == 'student') {
+    let sql = 'select * from 350_group_project_1.student_personal_information where username = ?;';
+    connection.query(sql,[req.body.username], (error, results, fields) => {
+      console.log(results[0]);
+      console.log(req.body);
+      // compare the data in database and the data user input
 
+      if (results[0].first_name != req.body.first_name || results[0].last_name != req.body.last_name || results[0].phone_no != req.body.phone_no || results[0].ec_name != req.body.ec_name || results[0].ec_phone_no != req.body.ec_phone_no || results[0].ec_relation != req.body.ec_relation) {
+        let sql = 'SET SQL_SAFE_UPDATES=0;UPDATE 350_group_project_1.student_personal_information SET first_name = ?, last_name = ?, phone_no = ?, ec_name = ?, ec_phone_no = ?, ec_relation = ? WHERE username = ?;SET SQL_SAFE_UPDATES=1;';
+        connection.query(sql, [req.body.first_name, req.body.last_name, req.body.phone_no, req.body.ec_name, req.body.ec_phone_no, req.body.ec_relation, req.body.username], (error, results, fields) => {
+          if (error) throw error;
+          if (results[1].affectedRows) {
+            res.render('student_role_student_info.ejs',
+              {
+                username: req.session.username,
+                role: req.session.role,
+                results: req.body
+              });
+          }
+          else {
+            res.redirect('/');
+          }
+        })
+      }
+      else{
+        res.redirect('/personal_information');
+      }
+      })
+  }
+
+
+}
+);
 app.post("/search", (req, res) => {
   if (req.session.authenticated) {
     const username = req.session.username;
@@ -152,6 +187,44 @@ app.get('/home', (req, res) => {
               username: req.session.username,
               role: req.session.role,
               results: results
+            });
+        }
+      })
+    }
+    else if (req.session.role == 'teacher') {
+      let sql = 'SELECT * FROM 350_group_project_1.academic_records ORDER BY student_id asc, program_id asc';
+      connection.query(sql, [username], (error, results, fields) => {
+        if (error) throw error;
+        else {
+          res.render('teacher_home.ejs',
+            {
+              username: req.session.username,
+              role: req.session.role,
+              results: results,
+            });
+        }
+      })
+    }
+  }
+});
+
+app.get('/personal_information', (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect('/');
+  }
+  else if (req.session.authenticated) {
+    const username = req.session.username;
+    if (req.session.role == 'student') {
+      let sql = 'SELECT * FROM 350_group_project_1.student_personal_information where username = ?;';
+      connection.query(sql, [username], (error, results, fields) => {
+        if (error) throw error;
+        else {
+          // console.log(results);
+          res.render('student_role_student_info.ejs',
+            {
+              username: req.session.username,
+              role: req.session.role,
+              results: results[0]
             });
         }
       })
