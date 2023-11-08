599747f999cfac35885ec095c49d47845a63d7f2
Yiu Hei Chang <89966239+jackyyhc@users.noreply.github.com>
2023-11-08 17:36:25 +0800

    add header, footer, update with new databasen UI

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/public'));
 
 const connection = mysql.createConnection({
   host: "139.162.49.133",
-  user: "admin",
-  password: "Jeffisthebest.01",
-  database: "350_group_project_1",
+  user: "350_group_project_user",
+  password: "jeffisthebest",
+  database: "350_group_project",
   port: "3306",
   multipleStatements: true,
 });
@@ -51,27 +51,27 @@ app.post("/password_modification", (req, res) => {
   if (!req.session.authenticated) {
     res.redirect('/');
   } else if (req.session.role == 'teacher' || req.session.role == 'student') {
-    let sql = 'select password from 350_group_project_1.user where username = ?;';
+    let sql = 'select password from 350_group_project.User where userId = ?;';
     connection.query(sql, [req.body.student_id], (error, results, fields) => {
       if (error) throw error;
       const orginal_pw = results[0].password;
       const username = req.body.student_id;
       const password = req.body.new_password;
       if (req.body.password != orginal_pw) {
-        res.render('password_issue.ejs', {message: "Password Incorrect"});
+        res.render('password_issue.ejs', { message: "Password Incorrect" });
       }
       else if (req.body.new_password != req.body.confirm_password) {
-        res.render('password_issue.ejs', {message: "Password Not Match"});
+        res.render('password_issue.ejs', { message: "Password Not Match" });
       }
       else if (req.body.new_password == "" || password.length < 8) {
-        res.render('password_issue.ejs', {message: "Password Not Satisfied Rule"});
+        res.render('password_issue.ejs', { message: "Password Not Satisfied Rule" });
       }
       else {
-        let sql = 'SET SQL_SAFE_UPDATES=0;UPDATE 350_group_project_1.user SET password = ? WHERE username = ?;SET SQL_SAFE_UPDATES=1;';
+        let sql = 'SET SQL_SAFE_UPDATES=0;UPDATE 350_group_project.User SET password = ? WHERE userId = ?;SET SQL_SAFE_UPDATES=1;';
         connection.query(sql, [password, username], (error, results, fields) => {
           if (error) throw error;
           if (results[1].affectedRows) {
-            res.render('password_issue.ejs', {message: "Password Change Successful"});
+            res.render('password_issue.ejs', { message: "Password Change Successful" });
           }
           else {
             res.redirect('/');
@@ -83,16 +83,18 @@ app.post("/password_modification", (req, res) => {
 }
 );
 
+
+// need edit again
 app.post("/submit-student-info", (req, res) => {
   if (!req.session.authenticated) {
     res.redirect('/');
   } else if (req.session.role == 'teacher' || req.session.role == 'student') {
-    let sql = 'select * from 350_group_project_1.student_personal_information where username = ?;';
+    let sql = 'select * from 350_group_project.StudentInfo where username = ?;';
     connection.query(sql, [req.body.username], (error, results, fields) => {
       // compare the data in database and the data user input
       if (results[0].first_name != req.body.first_name || results[0].last_name != req.body.last_name || results[0].phone_no != req.body.phone_no || results[0].ec_name != req.body.ec_name || results[0].ec_phone_no != req.body.ec_phone_no || results[0].ec_relation != req.body.ec_relation) {
-        let sql = 'SET SQL_SAFE_UPDATES=0;UPDATE 350_group_project_1.student_personal_information SET first_name = ?, last_name = ?, phone_no = ?, ec_name = ?, ec_phone_no = ?, ec_relation = ? WHERE username = ?;SET SQL_SAFE_UPDATES=1;';
-        connection.query(sql, [req.body.first_name, req.body.last_name, req.body.phone_no, req.body.ec_name, req.body.ec_phone_no, req.body.ec_relation, req.body.username], (error, results, fields) => {
+        let sql = 'SET SQL_SAFE_UPDATES=0;UPDATE 350_group_project.StudentInfo SET phone_no = ?, ec_name = ?, ec_phone_no = ?, ec_relation = ? WHERE username = ?;SET SQL_SAFE_UPDATES=1;';
+        connection.query(sql, [req.body.phone_no, req.body.ec_name, req.body.ec_phone_no, req.body.ec_relation, req.body.username], (error, results, fields) => {
           if (error) throw error;
           if (results[1].affectedRows) {
             res.render('student_role_student_info.ejs',
@@ -152,17 +154,18 @@ app.post("/search", (req, res) => {
 
 
 app.post('/login', (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const username = req.body.username;
   const password = req.body.password;
-  let sql = 'SELECT * FROM 350_group_project_1.user where username = ? and password = ?';
+  let sql = 'SELECT * FROM 350_group_project.User WHERE userId = ? AND password = ?;';
   connection.query(sql, [username, password], (error, results, fields) => {
     if (error) throw error;
     if (results.length > 0) {
       // set session and role
-      req.session.role = results[0].user_role;
+      console.log(results)
+      req.session.role = results[0].role;
       req.session.authenticated = true;
-      req.session.username = username;
+      req.session.username = results[0].userId;
       res.redirect('/home');
     } else {
       res.redirect('/');
@@ -170,22 +173,53 @@ app.post('/login', (req, res) => {
   });
 });
 
-app.get('/home', (req, res) => {
+app.get('/academic_record', (req, res) => {
   if (!req.session.authenticated) {
     res.redirect('/');
   }
   else if (req.session.authenticated) {
     const username = req.session.username;
     if (req.session.role == 'student') {
-      let sql = 'SELECT * FROM 350_group_project_1.academic_records where student_id = ? ORDER BY term asc, program_id asc';
+      let sql = 'SELECT * FROM 350_group_project.Study LEFT JOIN 350_group_project.Course ON 350_group_project.Study.courseCode = 350_group_project.Course.courseCode WHERE userId = ? ORDER BY 350_group_project.Study.studyYear asc, 350_group_project.Study.courseCode asc'
       connection.query(sql, [username], (error, results, fields) => {
         if (error) throw error;
         else {
-          res.render('student_home.ejs',
+          console.log(results);
+          res.render('stu_academic_record.ejs',
             {
               username: req.session.username,
               role: req.session.role,
               results: results
+            });
+        }
+      })
+    }
+  }
+})
+
+
+
+
+app.get('/home', (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect('/');
+  }
+  else if (req.session.authenticated) {
+    console.log("sec case of home")
+    const username = req.session.username;
+    console.log(req.session)
+    if (req.session.role == 'student') {
+      console.log(username)
+      let sql = 'SELECT usertable.userid, usertable.userfName, usertable.userlName, usertable.role,        350_group_project.StudentInfo.programme, 350_group_project.StudentInfo.admYear,        350_group_project.StudentInfo.admTerm, status_information.status_desc,        350_group_project.StudentInfo.standing FROM 350_group_project.User AS usertable INNER JOIN 350_group_project.StudentInfo ON usertable.userid = 350_group_project.StudentInfo.userId INNER JOIN 350_group_project.status_information ON 350_group_project.StudentInfo.status = status_information.status WHERE usertable.userId = ?;'
+      connection.query(sql, [username], (error, results, fields) => {
+        if (error) throw error;
+        else {
+          console.log(results[0]);
+          res.render('student_home.ejs',
+            {
+              username: req.session.username,
+              role: req.session.role,
+              results: results[0]
             });
         }
       })
@@ -214,7 +248,7 @@ app.get('/personal_information', (req, res) => {
   else if (req.session.authenticated) {
     const username = req.session.username;
     if (req.session.role == 'student') {
-      let sql = 'SELECT * FROM 350_group_project_1.student_personal_information where username = ?;';
+      let sql = 'SELECT * FROM 350_group_project.StudentInfo where username = ?;';
       connection.query(sql, [username], (error, results, fields) => {
         if (error) throw error;
         else {
@@ -263,9 +297,9 @@ app.get("/api/user", (req, res) => {
   // Perform a database query
   var search_string = ""
   if (req.query["username"] && req.query["password"]) {
-    search_string = " where `username` = " + req.query["username"] + " and `password` = " + req.query["password"]
+    search_string = " where `userId` = " + req.query["username"] + " and `password` = " + req.query["password"]
   }
-  connection.query("SELECT * FROM user" + search_string, (error, results, fields) => {
+  connection.query("SELECT * FROM User" + search_string, (error, results, fields) => {
     if (error) throw error;
     // Send the query results to the web page
     res.send(results);
@@ -303,5 +337,5 @@ app.get('/logout', (req, res) => {
 
 // Start the server on port 3000
 app.listen(80, () => {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 80");
 });
